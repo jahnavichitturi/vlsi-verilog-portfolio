e1c74cc20e8211abf0c02b1457a0a517b4559fc2
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 20:07:22 +0530

    Create not_gate_gatelevel_tb.v

---
+++ basic_verilog_gates/not_gate/gatelevel/not_gate_gatelevel_tb.v
@@ -0,0 +1,27 @@
+
+module not_gate_gatelevel_tb;
+
+reg a;
+wire y;
+
+not_gate_gatelevel uut (
+    .a(a),
+    .y(y)
+);
+
+initial
+begin
+
+    a = 0;
+    #10;
+
+    a = 1;
+    #10;
+
+    $finish;
+end
+  initial
+    begin
+      $monitor("sim time=%0t,a=%b,y=%b",$time,a,y);
+    end
+endmodule
