c421a953a944f866ddc89e5c87c8e2f565f45998
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 19:50:30 +0530

    Create or_gate_behavioral_tb.v

---
+++ basic_verilog_gates/or_gate/behavioral/or_gate_behavioral_tb.v
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+
+module or_gate_behavioral_tb;
+
+reg a;
+reg b;
+wire y;
+
+or_gate_behavioral uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial
+begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, or_gate_behavioral_tb);
+
+    $display("Time\t a\t b\t y");
+    $monitor("%0t\t %b\t %b\t %b", $time, a, b, y);
+
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+endmodule
