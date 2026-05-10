a61cc91ba46fd5e7aa7a2bac22406ed7587a210c
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 20:50:05 +0530

    Create testbench.v

---
+++ gates_using nandgate/and_gate/dataflow/testbench.v
@@ -0,0 +1,35 @@
+module and_using_nand_tb;
+
+reg a;
+reg b;
+wire y;
+
+and_using_nand_dataflow uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial
+begin
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+initial
+begin
+    $monitor("sim time=%0t, a=%b, b=%b, y=%b", $time, a, b, y);
+end
+
+endmodule
