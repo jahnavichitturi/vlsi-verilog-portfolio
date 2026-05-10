beabfbe55306da17a9ee3c71cceafe66fa77c21f
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 19:42:59 +0530

    Create and_gate_dataflow_tb.v

---
+++ basic_verilog_gates/and_gate/dataflow/and_gate_dataflow_tb.v
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+
+module and_gate_dataflow_tb;
+
+reg a;
+reg b;
+wire y;
+
+and_gate_dataflow uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial
+begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, and_gate_dataflow_tb);
+
+    $display("Time\t a\t b\t y");
+    $monitor("%0t\t %b\t %b\t %b", $time, a, b, y);
+
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+endmodule
