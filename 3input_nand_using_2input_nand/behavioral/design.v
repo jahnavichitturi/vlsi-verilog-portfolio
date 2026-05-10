b1def1261f8821b3cd3e2f06879a537d6bece7fa
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 21:17:24 +0530

    Update design.v

--- 3input_nand_using_2input_nand/behavioral/design.v
+++ 3input_nand_using_2input_nand/behavioral/design.v
@@ -1,10 +1,17 @@
-module nand3_using_nand2_dataflow(
+module nand3_using_nand2_behavioral(
     input a,
     input b,
     input c,
-    output y
+    output reg y
 );
 
-assign y = ~((~(a & b) & c));
+reg w1, w2;
+
+always @(*)
+begin
+    w1 = ~(a & b);
+    w2 = ~(w1 & c);
+    y  = ~(w2 & w2);
+end
 
 endmodule
