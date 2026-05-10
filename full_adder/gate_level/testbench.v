7c4638ff81db7234c48bce1f881acf29c5f01d29
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 21:26:18 +0530

    Create testbench.v

---
+++ full_adder/gate_level/testbench.v
@@ -0,0 +1,46 @@
+module full_adder_tb;
+
+reg a;
+reg b;
+reg cin;
+wire sum;
+wire cout;
+
+full_adder_gatelevel uut (
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .cout(cout)
+);
+
+initial
+begin
+    a = 0; b = 0; cin = 0;
+    #10;
+
+    a = 0; b = 0; cin = 1;
+    #10;
+
+    a = 0; b = 1; cin = 0;
+    #10;
+
+    a = 0; b = 1; cin = 1;
+    #10;
+
+    a = 1; b = 0; cin = 0;
+    #10;
+
+    a = 1; b = 1; cin = 1;
+    #10;
+
+    $finish;
+end
+
+initial
+begin
+    $monitor("sim time=%0t a=%b b=%b cin=%b sum=%b cout=%b",
+              $time, a, b, cin, sum, cout);
+end
+
+endmodulet
