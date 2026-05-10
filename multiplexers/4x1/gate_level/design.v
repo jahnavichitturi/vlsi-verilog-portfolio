e8a713f4a5ffb105fdcd7461e03856ac96b11a9b
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 21:45:53 +0530

    Create design.v

---
+++ multiplexers/4x1/gate_level/design.v
@@ -0,0 +1,24 @@
+module mux4x1_gatelevel(
+    input a,
+    input b,
+    input c,
+    input d,
+    input s1,
+    input s0,
+    output y
+);
+
+wire s1n, s0n;
+wire w0, w1, w2, w3;
+
+not(s1n, s1);
+not(s0n, s0);
+
+and(w0, a, s1n, s0n);
+and(w1, b, s1n, s0);
+and(w2, c, s1, s0n);
+and(w3, d, s1, s0);
+
+or(y, w0, w1, w2, w3);
+
+endmodule
