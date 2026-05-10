86bf80bb367348cb4c4e8ace2127574c185dfaee
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 20:28:17 +0530

    Create testbench.v

---
+++ basic_verilog_gates/xnor_gate/dataflow/testbench.v
@@ -0,0 +1,36 @@
+module xnor_gate_dataflow_tb;
+
+reg a;
+reg b;
+wire y;
+
+xnor_gate_dataflow uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial
+begin
+
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+initial
+begin
+    $monitor("sim time=%0t,a=%b,b=%b,y=%b",$time,a,b,y);
+end
+
+endmodule
