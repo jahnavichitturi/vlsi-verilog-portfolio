b755811a20a8a70873ed392a1582ee8bbe0ef52f
jahnavi chitturi <jahnavi.chitturi2003@gmail.com>
2026-05-10 20:54:26 +0530

    Create testbench.v

---
+++ gates_using nandgate/not gate/behavioral/testbench.v
@@ -0,0 +1,27 @@
+module not_using_nand_tb;
+
+reg a;
+wire y;
+
+not_using_nand_behavioral uut (
+    .a(a),
+    .y(y)
+);
+
+initial
+begin
+    a = 0;
+    #10;
+
+    a = 1;
+    #10;
+
+    $finish;
+end
+
+initial
+begin
+    $monitor("sim time=%0t, a=%b, y=%b", $time, a, y);
+end
+
+endmodule
